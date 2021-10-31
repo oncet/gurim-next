e58a1172ad343012f2b92e1d1489eb42f3872025
oncet <camilorivera86@gmail.com>
2021-10-31 00:22:38 -0300

    use chakra components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ChakraProvider, Container } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Container>
+        <main>
+          <Component {...pageProps} />
+        </main>
+        {/* <footer>
+          <p>Footer</p>
+        </footer> */}
+      </Container>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
