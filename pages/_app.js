08640360c08e7801dcaf0179c23b0e0431a5cd51
Camilo Rivera <crivera@srpago.com>
2021-11-06 23:19:08 -0300

    add custom colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,37 @@
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("yellow.50", "gray.800")(props),
+    },
+  }),
+};
+
+const colors = {
+  yellow: {
+    50: "#fffce5",
+    100: "#fef7b9",
+    200: "#fff189",
+    300: "#ffeb5a",
+    400: "#ffe635",
+    500: "#e6cc26",
+    600: "#b39f1d",
+    700: "#807113",
+    800: "#4d4407",
+    900: "#1a1700",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  styles,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Container>
         <main>
           <Component {...pageProps} />
