27c5dd909557ee51531eab86020e786668cf5497
oncet <camilorivera86@gmail.com>
2021-12-06 23:53:26 -0300

    change some margins

--- components/Tags.js
+++ components/Tags.js
@@ -2,7 +2,7 @@ import NextLink from "next/link";
 import { HStack, Tag, Link } from "@chakra-ui/react";
 
 const Tags = ({ tags }) => (
-  <HStack mb="2" spacing="2">
+  <HStack mb="3" spacing="2">
     {tags.map((tag) => (
       <Tag key={tag.slug} variant="ghost" size="sm" fontWeight="200">
         <Link as={NextLink} href={`/category/${tag.slug}`}>
