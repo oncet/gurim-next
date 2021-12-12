263fd24ea6d08ce34b9cd29d5235021e3784d5a8
oncet <camilorivera86@gmail.com>
2021-12-12 18:29:50 -0300

    create items grid component

---
+++ components/ItemsGrid.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import NextLink from "next/link";
+import NextImage from "next/image";
+import {
+  Button,
+  Center,
+  Text,
+  Image,
+  Link,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+import { getCategory } from "../lib/api";
+
+const AnimatedGridItem = motion(GridItem);
+
+const ItemsGrid = ({ initialItems, slug }) => {
+  const [endCursor, setEndCursor] = useState(initialItems.pageInfo.endCursor);
+  const [hasNextPage, setHasNextPage] = useState(
+    initialItems.pageInfo.hasNextPage
+  );
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [items, setItems] = useState(initialItems.edges);
+
+  const showMoreHandler = async () => {
+    setIsSubmitting(true);
+
+    const response = await getCategory(slug, endCursor);
+
+    setIsSubmitting(false);
+
+    setItems([...items, ...response.products.edges]);
+    setEndCursor(response.products.pageInfo.endCursor);
+    setHasNextPage(response.products.pageInfo.hasNextPage);
+  };
+
+  return (
+    <>
+      <Grid
+        templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
+        gap={6}
+      >
+        {items.map(({ node }) => (
+          <AnimatedGridItem
+            key={node.slug}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.6,
+              ease: "easeOut",
+            }}
+          >
+            <Link as={NextLink} href={`/product/${node.slug}`}>
+              <a>
+                <NextImage
+                  as={Image}
+                  src={node.featuredImage.node.sourceUrl}
+                  alt={node.title}
+                  width={350}
+                  height={350}
+                  layout="responsive"
+                />
+                <Text>{node.title}</Text>
+              </a>
+            </Link>
+          </AnimatedGridItem>
+        ))}
+      </Grid>
+      {hasNextPage && (
+        <Center mt="4">
+          <Button
+            isLoading={isSubmitting}
+            variant="ghost"
+            onClick={showMoreHandler}
+            disabled={isSubmitting}
+            fontWeight="400"
+          >
+            Mostrar mas
+          </Button>
+        </Center>
+      )}
+    </>
+  );
+};
+
+export default ItemsGrid;
--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,45 +1,12 @@
 import { useState } from "react";
 import Head from "next/head";
-import NextLink from "next/link";
-import NextImage from "next/image";
-import {
-  Button,
-  Center,
-  Text,
-  Heading,
-  Image,
-  Link,
-  Grid,
-  GridItem,
-} from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { Heading } from "@chakra-ui/react";
 
 import { getCategory, getCategoriesSlugs } from "../../lib/api";
-
-const AnimatedGridItem = motion(GridItem);
+import ItemsGrid from "../../components/ItemsGrid";
 
 export default function Category({ category, preview }) {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [moreResults, setMoreResults] = useState([]);
-  const [endCursor, setEndCursor] = useState();
-  const [hasNextPage, setHasNextPage] = useState(
-    category.products.pageInfo.hasNextPage
-  );
-
-  const showMoreHandler = async () => {
-    setIsSubmitting(true);
-
-    const response = await getCategory(
-      category.slug,
-      endCursor || category.products.pageInfo.endCursor
-    );
-
-    setIsSubmitting(false);
-
-    setMoreResults([...moreResults, ...response.products.edges]);
-    setEndCursor(response.products.pageInfo.endCursor);
-    setHasNextPage(response.products.pageInfo.hasNextPage);
-  };
+  console.log("category", category);
 
   return (
     <>
@@ -52,55 +19,7 @@ export default function Category({ category, preview }) {
         {category.name}
       </Heading>
       {category.products.edges && (
-        <>
-          <Grid
-            templateColumns={[
-              "repeat(1, 1fr)",
-              "repeat(2, 1fr)",
-              "repeat(3, 1fr)",
-            ]}
-            gap={6}
-          >
-            {[...category.products.edges, ...moreResults].map(({ node }) => (
-              <AnimatedGridItem
-                key={node.slug}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{
-                  duration: 0.6,
-                  ease: "easeOut",
-                }}
-              >
-                <Link as={NextLink} href={`/product/${node.slug}`}>
-                  <a>
-                    <NextImage
-                      as={Image}
-                      src={node.featuredImage.node.sourceUrl}
-                      alt={node.title}
-                      width={350}
-                      height={350}
-                      layout="responsive"
-                    />
-                    <Text>{node.title}</Text>
-                  </a>
-                </Link>
-              </AnimatedGridItem>
-            ))}
-          </Grid>
-          {hasNextPage && (
-            <Center mt="4">
-              <Button
-                isLoading={isSubmitting}
-                variant="ghost"
-                onClick={showMoreHandler}
-                disabled={isSubmitting}
-                fontWeight="400"
-              >
-                Mostrar mas
-              </Button>
-            </Center>
-          )}
-        </>
+        <ItemsGrid initialItems={category.products} slug={category.slug} />
       )}
     </>
   );
