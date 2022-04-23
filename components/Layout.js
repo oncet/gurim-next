f5c764cd2c76a678fddf41b22708b3d4bdffdfa5
crivera6 <crivera@srpago.com>
2022-04-23 17:35:50 -0300

    animate page layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { chakra, Box, Stack } from "@chakra-ui/react";
-import Footer from "./Footer"
+import { motion } from "framer-motion";
+import Footer from "./Footer";
 
 import Header from "./Header";
 
@@ -12,13 +13,22 @@ const Layout = ({ children }) => {
         <meta name="description" content="Author of handcrafted products" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box>
-        <Stack spacing="6" minHeight="100vh">
-          <Header />
-          <chakra.main flexGrow={1}>{children}</chakra.main>
-          <Footer />
-        </Stack>
-      </Box>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+      >
+        <Box>
+          <Stack spacing="6" minHeight="100vh">
+            <Header />
+            <chakra.main flexGrow={1}>{children}</chakra.main>
+            <Footer />
+          </Stack>
+        </Box>
+      </motion.div>
     </>
   );
 };
