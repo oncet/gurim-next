4eef12755658f31333f8167c1d06d47497163e59
oncet <camilorivera86@gmail.com>
2022-12-03 00:51:48 -0300

    feat: shorten slider transition

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home({ posts }) {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.15 }}
                 width="100%"
               >
                 <Image
