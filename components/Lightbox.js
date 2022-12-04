df249de6a7629212533d24324a28ff79223e18a1
Camilo <camilorivera86@gmail.com>
2022-12-04 20:20:43 -0300

    feat: remove chakra from lightbox component

--- components/Lightbox.js
+++ components/Lightbox.js
@@ -1,8 +1,4 @@
 import { useCallback, useEffect } from "react";
-import { Box, Image, Spinner } from "@chakra-ui/react";
-import { AnimatePresence, motion } from "framer-motion";
-
-const AnimatedBox = motion(Box);
 
 const Lightbox = ({
   imageCount,
@@ -76,42 +72,24 @@ const Lightbox = ({
   }, [onExit]);
 
   return (
-    <Box
-      background="rgba(0, 0, 0, 0.8)"
-      position="fixed"
-      top={0}
-      left={0}
-      height="100%"
-      width="100%"
+    <div
+      className="bg-black/80 fixed inset-0 flex items-center justify-center"
       onClick={onExit}
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
     >
-      <AnimatePresence>
-        <AnimatedBox
-          key={selectedImage.index}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-          position="absolute"
-        >
-          <Image
-            maxH="100vh"
-            cursor="pointer"
-            src={selectedImage.src}
-            alt={selectedImage.alt}
-            fallback={<Spinner mx="auto" color="white" />}
-            onClick={(event) => {
-              event.stopPropagation();
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        key={selectedImage.index}
+        className="max-h-screen cursor-pointer"
+        src={selectedImage.src}
+        alt={selectedImage.alt}
+        // fallback={<Spinner mx="auto" color="white" />}
+        onClick={(event) => {
+          event.stopPropagation();
 
-              onNavigate(event.type);
-            }}
-          />
-        </AnimatedBox>
-      </AnimatePresence>
-    </Box>
+          onNavigate(event.type);
+        }}
+      />
+    </div>
   );
 };
 
