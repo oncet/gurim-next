52c4c6ae3f0b1539513a9bc6a52a46d270cc3ce5
oncet <camilorivera86@gmail.com>
2022-12-31 18:48:54 -0300

    feat: finish removing chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import Head from "next/head";
 import Script from "next/script";
 import { useRouter } from "next/router";
-import { Center, ChakraProvider, Spinner } from "@chakra-ui/react";
-import { AnimatePresence, motion } from "framer-motion";
 
-import theme from "../theme";
 import Layout from "../components/Layout";
 import previewImage from "../public/images/preview.jpg";
 
@@ -35,48 +32,31 @@ function MyApp({ Component, pageProps }) {
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ChakraProvider theme={theme}>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=UA-147454552-1"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-147454552-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'UA-147454552-1');
         `}
-        </Script>
-        <Layout>
-          <AnimatePresence exitBeforeEnter>
-            <motion.div
-              key={router.asPath + router.isFallback}
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-              }}
-              exit={{
-                opacity: 0,
-              }}
-              transition={{
-                duration: 0.15,
-              }}
-            >
-              {router.isFallback ? (
-                <Center>
-                  <Spinner size="xl" thickness={4} emptyColor="gray.200" />
-                </Center>
-              ) : (
-                <Component {...pageProps} />
-              )}
-            </motion.div>
-          </AnimatePresence>
-        </Layout>
-      </ChakraProvider>
+      </Script>
+      <Layout>
+        <div>
+          {router.isFallback ? (
+            <div className="flex items-center justify-center">
+              {/* <Spinner size="xl" thickness={4} emptyColor="gray.200" /> */}
+              Loading...
+            </div>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </div>
+      </Layout>
     </>
   );
 }
