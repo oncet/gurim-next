dcbccca25881a05a59f9a276377589e56961c729
Camilo <camilorivera86@gmail.com>
2023-01-10 15:51:08 -0300

    feat: animate blog menu

--- components/Nav.js
+++ components/Nav.js
@@ -34,9 +34,10 @@ const Nav = () => {
           <WrappedLink onClick={handleSubmenuClick} href="#">
             Blog
           </WrappedLink>
-          {isOpen && (
-            <div
-              className="
+          <div
+            className={`
+                transition-opacity
+                ${isOpen ? "opacity-100" : "opacity-0"}
                 border-gray-200
                 border-r-2 md:border-none
                 md:rounded
@@ -44,42 +45,41 @@ const Nav = () => {
                 overflow-hidden
                 relative md:absolute
                 z-10
-              "
-            >
-              <ul className="bg-yellow-50 text-right md:text-left py-2">
-                <li>
-                  <NextLink passHref href="/category/bordado">
-                    <a className="px-6 py-1 inline-block">Bordado</a>
-                  </NextLink>
-                </li>
-                <li>
-                  <NextLink passHref href="/category/fieltro">
-                    <a className="px-6 py-1 inline-block">Fieltro</a>
-                  </NextLink>
-                </li>
-                <li>
-                  <NextLink passHref href="/category/bolsos">
-                    <a className="px-6 py-1 inline-block">Bolsos</a>
-                  </NextLink>
-                </li>
-                <li>
-                  <NextLink passHref href="/category/ecoprint">
-                    <a className="px-6 py-1 inline-block">Ecoprint</a>
-                  </NextLink>
-                </li>
-                <li>
-                  <NextLink passHref href="/category/encuadernacion">
-                    <a className="px-6 py-1 inline-block">Encuadernación</a>
-                  </NextLink>
-                </li>
-                <li>
-                  <NextLink passHref href="/blog">
-                    <a className="px-6 py-1 inline-block">Ver todo</a>
-                  </NextLink>
-                </li>
-              </ul>
-            </div>
-          )}
+              `}
+          >
+            <ul className="bg-yellow-50 text-right md:text-left py-2">
+              <li>
+                <NextLink passHref href="/category/bordado">
+                  <a className="px-6 py-1 inline-block">Bordado</a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink passHref href="/category/fieltro">
+                  <a className="px-6 py-1 inline-block">Fieltro</a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink passHref href="/category/bolsos">
+                  <a className="px-6 py-1 inline-block">Bolsos</a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink passHref href="/category/ecoprint">
+                  <a className="px-6 py-1 inline-block">Ecoprint</a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink passHref href="/category/encuadernacion">
+                  <a className="px-6 py-1 inline-block">Encuadernación</a>
+                </NextLink>
+              </li>
+              <li>
+                <NextLink passHref href="/blog">
+                  <a className="px-6 py-1 inline-block">Ver todo</a>
+                </NextLink>
+              </li>
+            </ul>
+          </div>
         </li>
         <li>
           <NextLink passHref href="/contacto">
