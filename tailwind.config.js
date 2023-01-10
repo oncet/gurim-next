8371679b8575871efea995f54c24f1e1f2d25c65
Camilo <camilorivera86@gmail.com>
2023-01-10 15:58:48 -0300

    feat: custom yellow color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,6 +7,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      yellow: {
+        50: "#fffef6",
+        100: "#fef7b9",
+        200: "#fff189",
+        300: "#ffeb5a",
+        400: "#ffe635",
+        500: "#e6cc26",
+        600: "#b39f1d",
+        700: "#807113",
+        800: "#4d4407",
+        900: "#1a1700",
+      },
+    },
     extend: {},
   },
   plugins: [],
