1eb78bc245873a43539b01a27f978ea618282328
oncet <camilorivera86@gmail.com>
2023-01-15 01:05:06 -0300

    feat: add route transition and svg spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { Transition } from "@headlessui/react";
 
 import Layout from "../components/Layout";
 import previewImage from "../public/images/preview.jpg";
@@ -32,16 +33,44 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div>
+        <Transition
+          show
+          appear
+          key={router.asPath + router.isFallback}
+          enter="transition-all"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-all"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="duration-500"
+        >
           {router.isFallback ? (
             <div className="flex items-center justify-center">
-              {/* <Spinner size="xl" thickness={4} emptyColor="gray.200" /> */}
-              Loading...
+              <svg
+                viewBox="0 0 60 60"
+                className="w-20 h-w-20 animate-spin duration-75"
+              >
+                <circle
+                  cx="30"
+                  cy="30"
+                  r="25"
+                  className="stroke-gray-200"
+                  fill="transparent"
+                  strokeWidth="5"
+                />
+                <path
+                  d="M 30 5 A 25 25 0 0 1 55 30"
+                  className="stroke-gray-800"
+                  fill="transparent"
+                  strokeWidth="5"
+                />
+              </svg>
             </div>
           ) : (
             <Component {...pageProps} />
           )}
-        </div>
+        </Transition>
       </Layout>
     </>
   );
