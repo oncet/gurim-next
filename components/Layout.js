7221d7e81cdf5e7ea0f6d7a63f0b3a9429fdf0f8
Camilo <camilorivera86@gmail.com>
2023-01-17 23:46:14 -0300

    feat: add transition to layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,26 @@
+import { Transition } from "@headlessui/react";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col gap-10 bg-yellow-50">
-      <Header />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
+    <Transition
+      show
+      appear
+      enter="transition-opacity"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="min-h-screen flex flex-col gap-10 bg-yellow-50">
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </div>
+    </Transition>
   );
 };
 
